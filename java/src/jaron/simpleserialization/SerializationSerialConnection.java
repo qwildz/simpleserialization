4300b4d792897e30281ddcbdebef3460e01a20ba
unknown <resnarizki29@gmail.com>
2015-10-31 18:39:10 +0900

    Rewrite connection class to fit android system.

--- java/src/jaron/simpleserialization/SerializationSerialConnection.java
+++ java/src/jaron/simpleserialization/SerializationSerialConnection.java
@@ -1,54 +1,47 @@
 package jaron.simpleserialization;
-import java.util.ArrayList;
 
-import processing.core.PApplet;
-import processing.serial.Serial;
+import java.util.ArrayList;
 
 /**
  * The <code>SerializationSerialConnection</code> class provides the functionality
  * to establish a serial connection and send and receive serialized data.
- *   
- * @author      jarontec gmail com
- * @version     1.0
- * @since       1.0
+ *
+ * @author jarontec gmail com
+ * @version 1.0
+ * @since 1.0
  */
-public class SerializationSerialConnection extends Thread implements Runnable {
-  private static final float DEFAULT_UPDATE_FREQUENCY = 10;
+public class SerializationSerialConnection {
 
-  private ArrayList<SerializationSerialReader> readers = new ArrayList<SerializationSerialReader>();
-  private Serial serial;
-  
-  public SerializationSerialConnection(PApplet applet, String port, int speed) {
-    serial = new Serial(applet, port, speed);
-    setDaemon(true);
-    start();
-  }
-  
-  public void addDeserializableData(SerializationData data) {
-    SerializationSerialReader reader = new SerializationSerialReader(data);
-    addReader(reader);
-  }
-  
-  public void addReader(SerializationSerialReader reader) {
-    readers.add(reader);
-  }
+    private ArrayList<SerializationSerialReader> readers = new ArrayList<SerializationSerialReader>();
 
-  @Override
-  public void run() {
-    while(true) {
-      while(serial.available() > 0) {
-        byte b = (byte )serial.read();
-        for (SerializationSerialReader reader : readers) { 
-          reader.processData(b);
-        }
-      }
-      try { sleep((long )(1000 / DEFAULT_UPDATE_FREQUENCY)); } catch(InterruptedException e) {} 
+    public SerializationSerialConnection() {
+
     }
-  }
-  
-  public void write(SerializationData data) {
-    SerializationOutputStream output = new SerializationOutputStream(data.getDataSize());
-    data.write(output);                 // serialize data and write it to a buffer
-    serial.write(output.toByteArray()); // write the buffer to the serial interface
-  }
+
+    public void addDeserializableData(SerializationData data) {
+        SerializationSerialReader reader = new SerializationSerialReader(data);
+        addReader(reader);
+    }
+
+    public void addReader(SerializationSerialReader reader) {
+        readers.add(reader);
+    }
+
+    public void read(byte b) {
+        for (SerializationSerialReader reader : readers) {
+            reader.processData(b);
+        }
+    }
+
+    public void read(byte[] b) {
+        for (int i = 0; i < b.length; i++) {
+            read(b[i]);
+        }
+    }
+
+    public byte[] write(SerializationData data) {
+        SerializationOutputStream output = new SerializationOutputStream(data.getDataSize());
+        data.write(output);                 // serialize data and write it to a buffer
+        return output.toByteArray();        // write the buffer to the serial interface
+    }
 }
